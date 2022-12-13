bd2a0c416f763b324ba1b9b2d12fc5e3b3d18360
mac tony <wdcvbnmk@outlook.com>
2022-12-13 19:25:42 +0800

    second order derivative

---
+++ test.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# fxprime=3y, fyprime=3x fxyprime=3e^x+3x*e^x 3(e+e)
+def func(x,y):
+    return 3*x*y*np.exp(x)
+
+
+
+x0=1
+y0=4
+start=1e-4
+stop=1e-1
+length=10
+xcordinate=np.geomspace(start, stop,length)
+dp=np.zeros((length,length))
+
+i=0
+j=0
+for h1 in xcordinate:
+    j=0
+    for h2 in xcordinate:
+        dp[i,j]=(func(x0+h1, y0+h2)-func(x0+h1, y0-h2)-func(x0-h1, y0+h2)+func(x0-h1, y0-h2))/(4*h1*h2)
+        j+=1
+    i+=1
+
+    
+
